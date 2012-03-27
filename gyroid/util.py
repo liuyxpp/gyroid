Before fixing the bug in Basis.sabf2fft and Basis.__init__. The bug is the wrong index for coefficient before each wave vectors.

gyroid/util.py
@@ -13,7 +13,7 @@ from mayavi import mlab
 
 __all__ = ["render_structure_1d","render_structure_2d","render_structure_3d"]
 
-def render_structure_1d(basis,Na,c,
+def render_structure_1d(basis,grid,Na,c,
                         save_data=True,data_name="struct1d.mat",
                         save_img=True,show_img=True,
                         img_name="struct1d.png",
@@ -24,7 +24,10 @@ def render_structure_1d(basis,Na,c,
     c     - coefficients for each basis function.
     """
 
-    struct = basis.generate_structure(Na,c)
+    #struct = basis.generate_structure(Na,c)
+    struct = basis.generate_structure_by_fft((Na,),c,grid)
+    # For debug only
+    print basis.fft2sabf(np.fft.fftn(struct),grid)
     a = 1.0 * basis.shape.h[0,0]
     rx = np.array([a*i/Na for i in np.arange(Na)])
 
@@ -52,7 +55,8 @@ def render_structure_2d(basis,grid,Na,Nb,c,
 
     #struct = basis.generate_structure((Na,Nb),c)
     struct = basis.generate_structure_by_fft((Na,Nb),c,grid)
-    print basis.fft2sabf(np.fft.fftn(struct),grid)
+    # For debug only
+    #print basis.fft2sabf(np.fft.fftn(struct),grid)
     rx = np.zeros((Na,Nb))
     ry = np.zeros((Na,Nb))
     for (i,j) in np.ndindex(Na,Nb):
@@ -108,7 +112,8 @@ def render_structure_3d(basis,grid,Na,Nb,Nc,c,
 
     #struct = basis.generate_structure((Na,Nb,Nc),c)
     struct = basis.generate_structure_by_fft((Na,Nb,Nc),c,grid)
-    print basis.fft2sabf(np.fft.fftn(struct),grid)
+    # For debug only
+    #print basis.fft2sabf(np.fft.fftn(struct),grid)
     rx = np.zeros((Na,Nb,Nc))
     ry = np.zeros((Na,Nb,Nc))
     rz = np.zeros((Na,Nb,Nc))

tests/test_basis.py
@@ -22,22 +22,37 @@ from gyroid import render_structure_3d
 def test_Basis1():
     b = "Bravais"
     uc = UnitCell(1,LAMELLAR,np.array([4.0]))
+    N1 = 128
 
     g1 = Group(1,b,uc.shape,"P1")
-    gd1 = Grid(np.array([4]),g1)
+    gd1 = Grid(np.array([N1]),g1)
     bs = Basis(g1,gd1)
-    render_structure_1d(bs,128,1.0)
     print bs.__dict__
     for s in bs.stars:
         print s.__dict__
+#    c = 1.0
+    c = np.zeros(bs.N)
+    c[0] = 1.0 * N1
+    c[1] = 6.0
+    c[2] = 4.0
+    c[3] = 0.5
+    c[4] = 3.0
+    render_structure_1d(bs,gd1,N1,c)
 
-    g2 = Group(1,b,uc.shape,"P-1")
-    gd2 = Grid(np.array([128]),g2)
-    bs2 = Basis(g2,gd2)
-    render_structure_1d(bs2,128,1.0)
-    print bs2.__dict__
-    for s in bs2.stars:
-        print s.__dict__
+#    g2 = Group(1,b,uc.shape,"P-1")
+#    gd2 = Grid(np.array([N1]),g2)
+#    bs2 = Basis(g2,gd2)
+#    print bs2.__dict__
+#    for s in bs2.stars:
+#        print s.__dict__
+#    c = 1.0
+#    c = np.zeros(bs2.N)
+#    c[0] = 1.0 * N1
+#    c[1] = 6.0
+#    c[2] = 1.0
+#    c[3] = 3.0
+#    c[4] = 0.2
+#    render_structure_1d(bs2,gd2,N1,c)
 
 def test_Basis2():
     b = "Bravais"
@@ -92,9 +107,9 @@ def test_Basis3():
 #    render_structure_3d(bs2,gd2,N1,N2,N3,c)
 
 def run_test():
-    #test_Basis1()
+    test_Basis1()
     #test_Basis2()
-    test_Basis3()
+    #test_Basis3()
 
 if __name__ == '__main__':
     run_test()